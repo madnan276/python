3996ab9ad96ab7ea86e9861bf8f32077982f8d21
madnan276 <164341271+madnan276@users.noreply.github.com>
2024-03-21 16:19:16 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,54 @@
+a = 10
+b = 5
+c = a+b
+print(c)
+
+
+a = 10
+b = 5
+c = a-b
+print(c)
+
+print(a > b and c > a)
+
+print(a > b or c > a)
+
+print(not a > b)
+
+print(id(a))
+
+marks = 40
+if marks >= 90 and marks >= 100:
+    print('Excellent')
+elif marks >= 80 and marks >= 90:
+    print('A+')
+elif marks >= 70 and marks >= 80:
+    print('A')
+elif marks >= 60 and marks >= 70:
+    print('B')
+elif marks >= 50 and marks >= 60:
+    print('C')
+else:
+    print('D')
+
+
+num = 000
+if num > 0:
+    print('it is a positive number')
+elif num == 0:
+    print('Number equal to zero')
+else:
+    print('its a negative number')
+
+
+
+
+year=int(2024)
+if year \4 and %=0:
+    print ('This is a leap year')
+else :
+print('This is not a leap year')
+
+
+list = [ '29' , '60' , '37' , '200']
+max = 0
